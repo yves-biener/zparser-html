ea759685e361bd3fce0c3dc8f2126ce30a73f800
Yves Biener <yves.biener@gmx.de>
2023-04-01 13:27:55 +0200

    add preprocessing methods

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,9 @@
+const Self = @This();
+
+input_stream: []const u8 = undefined,
+/// Insertion Point in the input_stream.
+point: usize = undefined,
+
 /// Errors which are defined to be identified during the parsing of the syntax of HTML.
 /// For more details see (Parse errors)[https://html.spec.whatwg.org/multipage/parsing.html#parse-errors]
 export const SyntaxError = error{
@@ -51,3 +57,24 @@ export const SyntaxError = error{
     UnexpectedSolidusInTag,
     UnknownNamedCharacterReference,
 };
+
+export fn init(input_stream: []const u8) Self {
+    return .{
+        .input_stream = input_stream,
+    };
+}
+
+/// Return the first character that has not yet been *consumed*.
+/// Initially the first character of the input_stream is returned.
+/// If there is no next character null is returned.
+export fn next(self: *Self) ?u8 {
+    _ = self;
+    return null;
+}
+
+/// Return the last character to have been *consumed*.
+/// If there has not been any character *consumed* yet null is returned.
+export fn current(self: *Self) ?u8 {
+    _ = self;
+    return null;
+}
